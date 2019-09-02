91c1f499c05429768153de86db7cfc48803b27b9
mikis <mikis.huhu@gmail.com>
2019-09-02 19:50:07 +0800

    add user model

--- src/server/router/user.js
+++ src/server/router/user.js
@@ -1,6 +1,5 @@
 import express from "express";
 import utility from "utility";
-import * as User from "../model/model";
 import cookie from "cookie-parser";
+import {userModel} from "../model/user";
 const Router = express.Router();
-const user = User.getModel("user");
