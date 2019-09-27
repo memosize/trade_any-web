cb9d420a52cb1fc161d4a035bb44cfa1e65fe0cc
memosize <89068024@qq.com>
2019-09-27 23:22:29 +0800

    delete js file

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,16 @@
-
-import express from 'express'
-import mongoose from 'mongoose'
-// const express = require('express')
-// const mongoose = require('mongoose')
-import bodyParser from 'body-parser'
-import  cookieParser from 'cookie-parser'
-// const bodyParser = require('body-parser')
-// const cookieParser =  require('cookie-parser')
- import  Router from "./router/user";
+import express from "express";
+// import mongoose from "mongoose";
+// const express = require("express");
+const mongoose = require("mongoose");
+// import bodyParser from "body-parser";
+// import cookieParser from "cookie-parser";
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+//  import  Router from "./router/user";
 // const Router = require('./router/user')
 
 const DB_URL = "mongodb://127.0.0.1:27017/tradeAny";
-mongoose.connect(DB_URL).then((res) => {
+mongoose.connect(DB_URL).then((res: any) => {
   if (res) {
     console.log("connect success");
   } else {
@@ -19,13 +18,13 @@ mongoose.connect(DB_URL).then((res) => {
   }
 });
 const app = express();
-app.get('/', function (req, res) {
-  res.send('hello, express')
-})
+app.get("/", function(req: any, res: any) {
+  res.send("hello, express");
+});
 app.use(cookieParser());
 app.use(bodyParser());
-app.use('/user',Router.Router);
+// app.use('/user',Router.Router);
 
-app.listen(9093, function(req, res) {
+app.listen(9093, function(req: any, res: any) {
   console.log("node.js start port 9093");
 });
